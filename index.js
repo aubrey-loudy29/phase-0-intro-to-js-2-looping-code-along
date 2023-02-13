695ae893623a4093cffdca5d246c409fc4f7dc17
Aubrey Loudenslager <aubrey.d.webster@gmail.com>
2023-02-13 16:22:54 -0700

    commit tree 7810b5514e7480fd7874527ae9c1b6a092eea9d6

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(names) {
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+      }
+    return cards;
+  }
+  writeCards(names)
+
+
+  function countDown() {
+    let n = 10;
+    while (n >= 0) {
+        console.log(n--);
+    }
+  }
+  countDown()
